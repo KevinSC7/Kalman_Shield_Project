e6728a3689f6ed949be787d48a41e5e70ada021f
KevinSC <Kevin@gmailfalso.com>
2022-03-11 18:25:51 +0100

    Creación del entorno (II)

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//INSTALACION IDE: https://www.luisllamas.es/como-programar-arduino-con-visual-studio-code-y-plaftormio/
+#include <Arduino.h>
+
+void setup() {
+  Serial.printf("hola");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
